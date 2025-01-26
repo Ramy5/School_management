72b13d72b9e4a16da528fddf529fc6f91b4dad6d
ramy sabry <ramyalsabry.dev@gmail.com>
2025-01-27 01:52:53 +0200

    initial commit, and create menu sidebar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,39 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "School Management Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
+        <Link
+          href={"/"}
+          className="flex p-4 justify-center items-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={28} height={28} />
+          <span className="hidden lg:block font-bold">School</span>
+        </Link>
+
+        <Menu />
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-auto">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(dashboard)/student/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const studentPage = () => {
+  return <div>student page</div>;
+};
+
+export default studentPage;
